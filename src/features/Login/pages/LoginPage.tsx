334de2085979536c581ee5d69e8b996dfb33075f
gabygabylagaby <gabrielagv7656@gmail.com>
2025-08-13 16:37:35 -0400

    Initial commit

---
+++ src/features/Login/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+      <p>Por favor, inicia sesión para continuar.</p>
+    </div>
+  );
+};
+
+export default LoginPage;
